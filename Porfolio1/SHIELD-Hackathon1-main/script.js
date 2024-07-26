90558cab69bfbdf7bf29acf9c946096e5d032fd1
Ibe Ezenwa Augustine <austinibe15@gmail.com>
2024-07-26 04:47:24 -0700

    update

--- Porfolio1/SHIELD-Hackathon1-main/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {  
-  const contactForm = document.getElementById('contactForm');  
-  contactForm.addEventListener('submit', function (event) {  
-      event.preventDefault();  
-      alert('Thank you for your message! I will get back to you shortly.');  
-      contactForm.reset(); // Reset the form after submission  
-  });  
-});
